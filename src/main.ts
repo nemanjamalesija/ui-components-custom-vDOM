83ada7cee053efc4bda8caddea0207ab7220b656
nemanjamalesija <malesija13@gmail.com>
2023-07-11 19:52:42 +0200

    Second commit

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,40 @@
 import './style.css';
 
-function generateData() {
-  let post: string = '';
-
-  function inputHandler(e: InputEvent) {
-    if (!e.target) return;
-
-    post = (e.target as HTMLInputElement).value;
-  }
-
-  return { post, inputHandler };
-}
-
-const { post, inputHandler } = generateData();
-
 // query selectors
 const input = document.querySelector('.input') as HTMLInputElement;
 const div = document.querySelector('.div');
+const submitButton = document.querySelector('.s-button');
+
+let post: string = '';
+const posts: string[] = [];
+
+function inputHandler(e: InputEvent) {
+  if (!e.target) return;
+
+  post = (e.target as HTMLInputElement).value;
+  inputToView();
+}
+
+function addPost(currentPost: string, e: Event) {
+  e.preventDefault();
+  posts.push(currentPost);
+  console.log(posts);
+  appendPosts();
+  post = '';
+  inputToView();
+}
 
 // event listeners
 input!.addEventListener('input', inputHandler as EventListener);
 input!.addEventListener('click', () => {
   input.value = '';
+  post = '';
 });
+submitButton!.addEventListener('click', (e) => addPost(post, e));
 
-function updateDOM() {
+// handle DOM input
+function inputToView() {
+  input.focus();
   if (!post) input.value = "What's on your mind ?";
   else {
     input!.value = post;
@@ -32,4 +42,15 @@ function updateDOM() {
   }
 }
 
-setTimeout(updateDOM, 15);
+// handle DOM divs
+function appendPosts() {
+  if (posts.length > 0) {
+    const newDiv = document.createElement('div');
+    const currPostIndex = posts.length - 1;
+    newDiv.textContent = posts[currPostIndex];
+    console.log(currPostIndex);
+    document.body.appendChild(newDiv);
+  }
+}
+
+inputToView();
