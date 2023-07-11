665384a81994930cda205fa718e1527b60f3021c
nemanjamalesija <malesija13@gmail.com>
2023-07-11 20:30:38 +0200

    Third commit

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,84 @@
 import './style.css';
 
-// query selectors
-const input = document.querySelector('.input') as HTMLInputElement;
-const div = document.querySelector('.div');
-const submitButton = document.querySelector('.s-button');
+// // query selectors
+// const input = document.querySelector('.input') as HTMLInputElement;
+// const div = document.querySelector('.div');
+// const submitButton = document.querySelector('.s-button');
 
-let post: string = '';
-const posts: string[] = [];
+// let post: string = '';
+// const posts: string[] = [];
+
+// function inputHandler(e: InputEvent) {
+//   if (!e.target) return;
+
+//   post = (e.target as HTMLInputElement).value;
+//   inputToView();
+// }
+
+// function addPost(currentPost: string, e: Event) {
+//   e.preventDefault();
+//   posts.push(currentPost);
+//   console.log(posts);
+//   appendPosts();
+//   post = '';
+//   inputToView();
+// }
+
+// // event listeners
+// input!.addEventListener('input', inputHandler as EventListener);
+// input!.addEventListener('click', () => {
+//   input.value = '';
+//   post = '';
+// });
+// submitButton!.addEventListener('click', (e) => addPost(post, e));
+
+// // handle DOM input
+// function inputToView() {
+//   input.focus();
+//   if (!post) input.value = "What's on your mind ?";
+//   else {
+//     input!.value = post;
+//     div!.textContent = post;
+//   }
+// }
+
+// // handle DOM divs
+// function appendPosts() {
+//   if (posts.length > 0) {
+//     const newDiv = document.createElement('div');
+//     const currPostIndex = posts.length - 1;
+//     newDiv.textContent = posts[currPostIndex];
+//     console.log(currPostIndex);
+//     document.body.appendChild(newDiv);
+//   }
+// }
+
+// inputToView();
+
+let post = '';
+let jsInput: HTMLInputElement;
+let jsDiv: HTMLDivElement | '';
 
 function inputHandler(e: InputEvent) {
   if (!e.target) return;
 
   post = (e.target as HTMLInputElement).value;
-  inputToView();
 }
 
-function addPost(currentPost: string, e: Event) {
-  e.preventDefault();
-  posts.push(currentPost);
-  console.log(posts);
-  appendPosts();
-  post = '';
-  inputToView();
+function dataToView() {
+  jsInput = document.createElement('input');
+
+  jsDiv = post == 'Will' ? '' : document.createElement('div');
+
+  jsInput.value = post;
+
+  if (jsDiv) jsDiv.textContent = post;
+
+  document.body.replaceChildren(jsInput, jsDiv as HTMLDivElement);
+
+  jsInput.addEventListener('input', inputHandler as EventListener);
+
+  jsInput.focus();
 }
 
-// event listeners
-input!.addEventListener('input', inputHandler as EventListener);
-input!.addEventListener('click', () => {
-  input.value = '';
-  post = '';
-});
-submitButton!.addEventListener('click', (e) => addPost(post, e));
-
-// handle DOM input
-function inputToView() {
-  input.focus();
-  if (!post) input.value = "What's on your mind ?";
-  else {
-    input!.value = post;
-    div!.textContent = post;
-  }
-}
-
-// handle DOM divs
-function appendPosts() {
-  if (posts.length > 0) {
-    const newDiv = document.createElement('div');
-    const currPostIndex = posts.length - 1;
-    newDiv.textContent = posts[currPostIndex];
-    console.log(currPostIndex);
-    document.body.appendChild(newDiv);
-  }
-}
-
-inputToView();
+setInterval(dataToView, 15);
