a8da1f4f4df28b46b5a39c4f5e7df963d29e6b59
nemanjamalesija <malesija13@gmail.com>
2023-07-12 01:08:22 +0200

    Add diffing algoritam. Protect data

--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,25 @@
 import './style.css';
 
-// // query selectors
-// const input = document.querySelector('.input') as HTMLInputElement;
-// const div = document.querySelector('.div');
-// const submitButton = document.querySelector('.s-button');
-
-// let post: string = '';
-// const posts: string[] = [];
-
-// function inputHandler(e: InputEvent) {
-//   if (!e.target) return;
-
-//   post = (e.target as HTMLInputElement).value;
-//   inputToView();
-// }
-
-// function addPost(currentPost: string, e: Event) {
-//   e.preventDefault();
-//   posts.push(currentPost);
-//   console.log(posts);
-//   appendPosts();
-//   post = '';
-//   inputToView();
-// }
-
-// // event listeners
-// input!.addEventListener('input', inputHandler as EventListener);
-// input!.addEventListener('click', () => {
-//   input.value = '';
-//   post = '';
-// });
-// submitButton!.addEventListener('click', (e) => addPost(post, e));
-
-// // handle DOM input
-// function inputToView() {
-//   input.focus();
-//   if (!post) input.value = "What's on your mind ?";
-//   else {
-//     input!.value = post;
-//     div!.textContent = post;
-//   }
-// }
-
-// // handle DOM divs
-// function appendPosts() {
-//   if (posts.length > 0) {
-//     const newDiv = document.createElement('div');
-//     const currPostIndex = posts.length - 1;
-//     newDiv.textContent = posts[currPostIndex];
-//     console.log(currPostIndex);
-//     document.body.appendChild(newDiv);
-//   }
-// }
-
-// inputToView();
-let vDOM;
-let elems;
-
 const data: {
   [key: string]: string;
 } = {
   post: '',
-  post2: '',
 };
 
 function updateData(label: string, value: string) {
   data[label] = value;
   updateDOM();
 }
+
+function generateData() {
+  let vDOM = createVDOM();
+  let prevDom: any[] = [];
+  let elems: any[] = [];
+
+  return { vDOM, prevDom, elems };
+}
+
+let { vDOM, prevDom, elems } = generateData();
 
 function inputHandler(e: InputEvent) {
   if (!e.target) return;
@@ -91,37 +43,38 @@ function createVDOM() {
 }
 
 function updateDOM() {
-  const vDOM = createVDOM();
-  const actualDOM = vDOM.map(convert);
-
-  document.body.replaceChildren(...(actualDOM as Node[]));
-
-  const jsInputDOM = actualDOM.find(
-    (element) => element instanceof HTMLInputElement
-  ) as HTMLInputElement;
-  if (jsInputDOM) {
-    jsInputDOM.focus();
+  if (!elems) {
+    elems = vDOM.map(convert);
+    document.body.replaceChildren(...(elems as Node[]));
+  } else {
+    prevDom = [...vDOM];
+    vDOM = createVDOM();
+    findDiff(prevDom, vDOM);
   }
 }
 
 function convert(vDOM: any[]) {
-  let convertedInput: HTMLInputElement;
-  let convertedElem: Element;
+  const element = document.createElement(vDOM[0]);
 
   if (vDOM[0] === 'input') {
-    convertedInput = document.createElement(vDOM[0]);
-    convertedInput.value = vDOM[1];
-    convertedInput.addEventListener('input', vDOM[2] as EventListener);
-    return convertedInput;
+    element.value = vDOM[1];
+    element.addEventListener('input', vDOM[2] as EventListener);
+    return element as HTMLInputElement;
   }
 
   if (vDOM[0] === 'div' || vDOM[0] === 'h3' || vDOM[0] === 'p') {
-    convertedElem = document.createElement(vDOM[0]);
-    convertedElem.textContent = vDOM[1];
-    return convertedElem;
+    element.textContent = vDOM[1];
+    return element as Element;
   } else return new Error('Item not convertible or incorrectly passed item');
 }
 
-// setInterval(updateDOM, 150);
+function findDiff(prevVDOM: any[], currentVDOM: any[]) {
+  for (let i = 0; i < currentVDOM.length; i++) {
+    if (JSON.stringify(prevVDOM[i]) !== JSON.stringify(currentVDOM[i])) {
+      elems[i].textContent = currentVDOM[i][1];
+      elems[i].value = currentVDOM[i][1];
+    }
+  }
+}
 
 updateDOM();
